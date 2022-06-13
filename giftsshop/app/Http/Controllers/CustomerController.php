c09d570a84c13f2a4f9603feba4c72e0047d0cbf
TuanVan61312 <92676025+TuanVan61312@users.noreply.github.com>
2022-06-13 22:30:13 +0700

    Add files via upload
    customer

--- giftsshop/app/Http/Controllers/CustomerController.php
+++ giftsshop/app/Http/Controllers/CustomerController.php
@@ -3,138 +3,64 @@
 namespace App\Http\Controllers;
 
 use App\Repository\CusRepos;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
+
 class CustomerController extends Controller
 {
-    public function index()
-    {
+    public function index(){
         $customer = CusRepos::getAllCus();
-        return view('AdminSite.customer.index',
-            [
-                'customer' => $customer
-            ]);
+        return view('giftshop.customer.index', [
+            'customer' => $customer
+        ]);
     }
 
-    public function show($Cus_id)
-    {
-        $customer = CusRepos::getCusById($Cus_id);
-        return view('AdminSite.customer.show',
-            [
-                'customer' => $customer[0]
-            ]
-        );
+    public function show($Cus_id){
+        $customer = CusRepos::getCustomerById($Cus_id);
+        return view('giftshop.customer.show', [
+            'customer' => $customer[0]
+        ]);
     }
 
-    public function loginCus()
-    {
-        return view('GiftsShop.Log&Reg.login',
-        ['customer' => (object)[
-            'Cus_Username' => '',
-            'Cus_Password' => ''
-        ]]);
+    public function edit($Cus_id){
+        $customer = CusRepos::getCustomerById($Cus_id);
+        return view('giftshop.customer.update', [
+            'customer' => $customer[0]
+        ]);
     }
 
-    public function registerCus()
-    {
-        return view(
-            'GiftsShop.Log&Reg.reg',
-            ['customer' => (object)[
-                'Cus_id' => '',
-                'Cus_Fullname' => '',
-                'Cus_Username' => '',
-                'Cus_Email' => '',
-                'Cus_DoB' => '',
-                'Phone' => '',
-                'Address' => ''
-            ]]);
-    }
-    public function store(Request $request)
-    {
-        $this->formValidateCus($request)->validate();
-
-        $customer = (object)[
-            'Cus_Fullname' => $request->input('Cus_Fullname'),
-            'Cus_Username' => $request->input('Cus_Username'),
-            'Cus_Email' => $request->input('Cus_Email'),
-            'Cus_DoB' => $request->input('Cus_DoB'),
-            'Phone' => $request->input('Phone'),
-            'Address' => $request->input('Address'),
-        ];
-
-        $newCus_Id = CusRepos::insert($customer);
-        return redirect()
-            ->route('GiftsShop.homepage')
-            ->with('msg', 'New customer with id: '.$newCus_Id.' has been Update');
-
-    }
-
-    public function edit($Cus_id)
-    {
-        $customer = CusRepos::getCusById($Cus_id);
-
-        return view(
-            'AdminSite.customer.update',
-            ["customer" => $customer[0]]);
-    }
-
-    public function update(Request $request, $Cus_id)
-    {
-        if ($Cus_id != $request->input('Cus_id')) {
+    public function update(Request $request, $Cus_id){
+        if($Cus_id != $request->input('Cus_id')){
             return redirect()->action('CustomerController@index');
         }
-
-        $this->formValidateCus($request)->validate();
-
+        $this->formValidate($request)->validate();
         $customer = (object)[
             'Cus_id' => $request->input('Cus_id'),
             'Cus_Fullname' => $request->input('Cus_Fullname'),
             'Cus_Username' => $request->input('Cus_Username'),
-            'Cus_Email' => $request->input('Cus_Email'),
+            'Cus_password' => $request->input('Cus_password'),
+            'Cus_gender' => $request->input('Cus_gender'),
+            'Cus_phone' => $request->input('Cus_phone'),
             'Cus_DoB' => $request->input('Cus_DoB'),
-            'Phone' => $request->input('Phone'),
-            'Address' => $request->input('Address'),
+            'Cus_add' => $request->input('Cus_add')
         ];
         CusRepos::update($customer);
-        return redirect()->action('CustomerController@index');
-
+        return redirect()->action('CustomerController@index')->with('msg', 'update successfully');
     }
 
-//    public function confirm($Cus_id){
-//        $customer = CusRepos::getCusById($Cus_id);
-//
-//        return view('AdminSite.customer.confirm',
-//            [
-//                'customer' => $customer[0]
-//            ]
-//        );
-//    }
-//
-//    public function destroy(Request $request, $Cus_id)
-//    {
-//        if ($request->input('Cus_id') != $Cus_id) {
-//            return redirect()->action('CustomerController@index');
-//        }
-//
-//        CusRepos::delete($Cus_id);
-//
-//        return redirect()->action('CustomerController@index');
-//    }
-
-    private function formValidateCus($request)
-    {
+    public function formValidate($request){
         return Validator::make(
             $request->all(),
             [
                 'Cus_Fullname' => ['required'],
                 'Cus_Username' => ['required'],
-                'Cus_Email' => ['required', 'email'],
-                'Cus_DoB' => ['required', 'date'],
-                'Phone' => ['required', 'max:10'],
-                'Address' => ['required'],
-            ],
+                'Cus_gender' => ['required'],
+                'Cus_phone' => ['required'],
+                'Cus_DoB' => ['required'],
+                'Cus_add' => ['required']
+            ]
         );
-
     }
 }
