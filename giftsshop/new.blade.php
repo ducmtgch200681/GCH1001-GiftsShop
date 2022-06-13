4d7ee43bd5ce93025678652f9fb0d45bbe1ea0e3
CVHoang0205 <106986621+CVHoang0205@users.noreply.github.com>
2022-06-14 01:32:04 +0700

    Add files via upload

---
+++ giftsshop/new.blade.php
@@ -0,0 +1,16 @@
+@extends('masters.adMaster')
+
+@section('main')
+    <div class="container">
+        <div class="p-2 pt-md-5 pb-md-3 mx-auto ">
+            <h1 class="display-4">New Category</h1>
+        </div>
+        @include('partials.error')
+
+        <form action="{{route('category.store')}}" method="post">
+            @csrf
+            @include('AdminSite.category.cateFields')
+            <button type="submit" class="btn btn-dark">Submit</button>
+        </form>
+    </div>
+@endsection
