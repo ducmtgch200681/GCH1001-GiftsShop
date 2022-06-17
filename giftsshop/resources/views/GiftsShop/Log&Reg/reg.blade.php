6792fa022463946866097445c6890e275a025663
ducmtgch200681 <ducmtgch200681@fpt.edu.vn>
2022-06-18 01:34:44 +0700

    ok

--- giftsshop/resources/views/GiftsShop/Log&Reg/reg.blade.php
+++ giftsshop/resources/views/GiftsShop/Log&Reg/reg.blade.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sign Up Form by Colorlib</title>
-
-    <!-- Font Icon -->
-    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
-
-    <!-- Main css -->
-    <link rel="stylesheet" href="css/stylelogreg.css">
-</head>
-<body>
-
-<div class="main">
-
-    <!-- Sign up form -->
+@extends('masters.logregMaster')
+@section('main')
+<!-- Sign up form -->
     <section class="signup">
         <div class="container">
             <div class="signup-content">
@@ -72,11 +56,4 @@
             </div>
         </div>
     </section>
-
-</div>
-
-<!-- JS -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="js/main.js"></script>
-</body>
-</html>
+@endsection
