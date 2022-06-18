616b88815e624eb0aed468d99fbffaa33712ad11
CVHoang0205 <106986621+CVHoang0205@users.noreply.github.com>
2022-06-18 09:51:23 +0700

    Add files via upload

---
+++ giftsshop/resources/views/AdminSite/gifts/update.blade.php
@@ -0,0 +1,17 @@
+@extends('masters.adMaster')
+
+@section('main')
+    <div class="container">
+        <br>
+        <div class="p-2 pt-md-5 pb-md-3 mx-auto ">
+            <h1 class="display-4">Update Gifts</h1>
+        </div>
+        @include('partials.errors')
+
+        <form action="{{route('product.update', ['Gifts_id'=> $product->Gifts_id])}}" method="post" enctype= "multipart/form-data">
+            @csrf
+            @include('AdminSite.gifts.proFields')
+            <button type="submit" class="btn btn-dark">Submit</button>
+        </form>
+    </div>
+@endsection
