dfada3bc4895e92600626b889c8e0e59ef865f6f
TuanVan61312 <92676025+TuanVan61312@users.noreply.github.com>
2022-06-25 07:11:24 +0700

    Add files via upload

---
+++ giftsshop/resources/views/GiftsShop/Mainpage/about.blade.php
@@ -0,0 +1,26 @@
+@extends('masters.giftsshopMaster')
+
+@section('main')
+    <div class="container">
+        <h3>
+        In the 4.0 technology era, the demand for online shopping is high and online shopping will help people save
+        a lot of time compared to going to the store. So our team decided to build a website where people would get
+        all the information about the different products available to them easily. Gifts Online is a project we created
+        to sell gifts so that buyers can buy directly on the website without having to go to the store. My team
+        advertises by distributing leaflets, television advertisements to attract people to our website selling gifts.
+        Frames and a good logo should be used to create the Home Page. Gift item descriptions and photographs
+        should be included on the home page. The site should have a menu with options for a quick introduction to
+        the many gift products available, the shop's location, and any other information needed.
+        Product information should be presented in one of two ways: Information should be available by category,
+        with the user being able to click on a certain category and see all of the products connected to that category
+        with complete information. For example, a category could be "mugs," "cards," "bracelets," and so on, with
+        appropriate sub-categories created. And product information should be organized by brand name. For
+        example, if a user wishes to see only "Hallmark" products or any other company's products, he or she can
+        click on a link/button/menu and see only those products.
+        If a user picks a brand, a list of products associated with that brand is provided. Different product qualities
+        should be summarized on the Website page alongside the product, but full features should be saved in
+        individual Word documents that may be downloaded or accessed by the User who desires to examine them.
+        The customer should also be able to compare products from other brands, both similar and dissimilar.
+        </h3>
+    </div>
+@endsection
---
+++ giftsshop/resources/views/GiftsShop/Mainpage/address.blade.php
@@ -0,0 +1,6 @@
+@extends('masters.giftsshopMaster')
+@section('main')
+    <div class="container">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d24155.40718652178!2d106.60709809920333!3d8.683425068038375!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3198a9266222ed9b%3A0xfb2d5b5bbbdef724!2zTmjDoCB0w7kgQ8O0biDEkOG6o28!5e0!3m2!1svi!2s!4v1656111508223!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+@endsection
